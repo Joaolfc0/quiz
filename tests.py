4107fd77349450caa2c82a4d22a81f666bd81b9d
Joaolfc0 <joaolfcerqueira@gmail.com>
2025-09-04 15:51:57 -0300

    Commit 2: Creating 10 unit tests

--- tests.py
+++ tests.py
@@ -34,3 +34,84 @@ def test_create_choice():
     assert len(question.choices) == 1
     assert choice.text == 'a'
     assert not choice.is_correct
+
+
+def test_add_multiple_choices_assigns_incremental_ids():
+    question = Question(title='q1')
+    c1 = question.add_choice('a')
+    c2 = question.add_choice('b')
+    c3 = question.add_choice('c')
+    assert [c1.id, c2.id, c3.id] == [1, 2, 3]
+
+
+def test_remove_choice_by_id_removes_choice():
+    question = Question(title='q1')
+    c1 = question.add_choice('a')
+    question.add_choice('b')
+    question.remove_choice_by_id(c1.id)
+    remaining_ids = [c.id for c in question.choices]
+    assert remaining_ids == [2]
+
+
+def test_remove_choice_by_id_invalid_raises():
+    question = Question(title='q1')
+    question.add_choice('a')
+    with pytest.raises(Exception):
+        question.remove_choice_by_id(999)
+
+
+def test_remove_all_choices_empties_list():
+    question = Question(title='q1')
+    question.add_choice('a')
+    question.add_choice('b')
+    question.remove_all_choices()
+    assert question.choices == []
+
+
+def test_set_correct_choices_marks_correct_flags():
+    question = Question(title='q1')
+    c1 = question.add_choice('a')
+    c2 = question.add_choice('b')
+    question.set_correct_choices([c2.id])
+    assert not question.choices[0].is_correct
+    assert question.choices[1].is_correct
+
+
+def test_set_correct_choices_with_invalid_id_raises():
+    question = Question(title='q1')
+    question.add_choice('a')
+    with pytest.raises(Exception):
+        question.set_correct_choices([999])
+
+
+def test_correct_selected_choices_exceeds_max_raises():
+    question = Question(title='q1', max_selections=1)
+    c1 = question.add_choice('a', is_correct=True)
+    c2 = question.add_choice('b', is_correct=False)
+    with pytest.raises(Exception):
+        question.correct_selected_choices([c1.id, c2.id])
+
+
+def test_correct_selected_choices_returns_only_correct():
+    question = Question(title='q1', max_selections=3)
+    c1 = question.add_choice('a', is_correct=True)
+    c2 = question.add_choice('b', is_correct=False)
+    c3 = question.add_choice('c', is_correct=True)
+    returned = question.correct_selected_choices([c1.id, c2.id, c3.id])
+    assert set(returned) == {c1.id, c3.id}
+
+
+def test_title_boundary_lengths_valid():
+    assert Question(title='a', points=1).title == 'a'
+    title_200 = 'x' * 200
+    assert Question(title=title_200, points=10).title == title_200
+
+
+def test_choice_text_boundaries():
+    question = Question(title='q1')
+    with pytest.raises(Exception):
+        question.add_choice('')
+    with pytest.raises(Exception):
+        question.add_choice('x' * 101)
+    ok = question.add_choice('x' * 100)
+    assert ok.text == 'x' * 100
